f2649a709236269d12d5698b520c2261731537fc
aritraChatterjee <aritra.cnw@gmail.com>
2020-04-18 23:15:47 +0200

    Updates

---
+++ precache-manifest.ab9b9910d5ebdfa6d05b2e471f404583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38e72bc7f44363745e881229b244a58",
+    "url": "/codes-and-wires/index.html"
+  },
+  {
+    "revision": "bda2fc641bf3187fd5f0",
+    "url": "/codes-and-wires/static/js/2.51e5955f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/codes-and-wires/static/js/2.51e5955f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4c454c52c01ebed8c2",
+    "url": "/codes-and-wires/static/js/main.2e6c7b70.chunk.js"
+  },
+  {
+    "revision": "c046d6f7175cdccca02b",
+    "url": "/codes-and-wires/static/js/runtime-main.1279ba40.js"
+  }
+]);
